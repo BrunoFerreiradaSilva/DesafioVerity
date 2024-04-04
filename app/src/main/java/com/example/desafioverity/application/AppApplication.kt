42f580c78eaa7536c122f9fa640899aba4c41a00
BrunoFerreiradaSilva <31040011+BrunoFerreiradaSilva@users.noreply.github.com>
2024-04-04 20:40:37 -0300

    include application for using hilt

---
+++ app/src/main/java/com/example/desafioverity/application/AppApplication.kt
@@ -0,0 +1,8 @@
+package com.example.desafioverity.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppApplication : Application() {
+}
