e4f63cb17c4a3fb25817723ac803a80a7426915d
BrunoFerreiradaSilva <31040011+BrunoFerreiradaSilva@users.noreply.github.com>
2024-04-06 10:33:11 -0300

    include extension for date.

---
+++ app/src/main/java/com/example/desafioverity/domain/helpers/DateExt.kt
@@ -0,0 +1,11 @@
+package com.example.desafioverity.domain.helpers
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun Date.showDataConvertingString():String{
+    val data = Date(this.time)
+    val simpleDateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+    return simpleDateFormat.format(data)
+}
