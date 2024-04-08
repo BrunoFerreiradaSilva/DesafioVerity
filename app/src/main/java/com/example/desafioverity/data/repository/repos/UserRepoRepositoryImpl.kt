ded971bb39130ea9827b3e8489f2d591af7e94b6
BrunoFerreiradaSilva <31040011+BrunoFerreiradaSilva@users.noreply.github.com>
2024-04-07 21:23:11 -0300

    update repositories for get remaining times

--- app/src/main/java/com/example/desafioverity/data/repository/repos/UserRepoRepositoryImpl.kt
+++ app/src/main/java/com/example/desafioverity/data/repository/repos/UserRepoRepositoryImpl.kt
@@ -1,23 +1,25 @@
 package com.example.desafioverity.data.repository.repos
 
+import com.example.desafioverity.data.repository.preferences.UserPreferenceRepository
 import com.example.desafioverity.domain.model.Repos
 import com.example.desafioverity.data.service.Service
 import com.example.desafioverity.domain.helpers.DataState
+import com.example.desafioverity.domain.helpers.LimitState
 import com.example.desafioverity.domain.helpers.LoadingState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class UserRepoRepositoryImpl @Inject constructor(private val service: Service) : UserRepoRepository {
+class UserRepoRepositoryImpl @Inject constructor(private val service: Service, private val userPreferenceRepository: UserPreferenceRepository) : UserRepoRepository {
     override fun getAllRepos(name: String): Flow<DataState<List<Repos>>> = flow {
         emit(DataState.Loading(LoadingState.Loading))
         try {
             val result = service.getAllRepos(name).map { repo ->
                 Repos(
                     id = repo.id,
-                    nodeId = repo.nodeId,
-                    name = repo.name,
-                    fullName = repo.fullName,
+                    nodeId = repo.nodeId?: "",
+                    name = repo.name?:"",
+                    fullName = repo.fullName?: "",
                     private = repo.private,
                     owner = repo.owner,
                     avatarUrl = repo.avatarUrl,
@@ -26,7 +28,7 @@ class UserRepoRepositoryImpl @Inject constructor(private val service: Service) :
                     language = repo.language,
                     forks = repo.forks,
                     watchers = repo.watchers,
-                    visibility = repo.visibility,
+                    visibility = repo.visibility?: "",
                     updatedAt = repo.updatedAt,
                     stargazersCount = repo.stargazersCount,
                     watchersCount = repo.watchersCount
@@ -35,6 +37,11 @@ class UserRepoRepositoryImpl @Inject constructor(private val service: Service) :
             emit(DataState.Data(data = result))
         } catch (error: Exception) {
             emit(DataState.Error(error = error))
+        }
+        userPreferenceRepository.getRemaining.collect {
+            if (it == "0"){
+                emit(DataState.Limit(LimitState.Limit))
+            }
         }
     }
 }
--- app/src/main/java/com/example/desafioverity/data/repository/users/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/desafioverity/data/repository/users/UserRepositoryImpl.kt
@@ -1,15 +1,16 @@
 package com.example.desafioverity.data.repository.users
 
+import com.example.desafioverity.data.repository.preferences.UserPreferenceRepository
 import com.example.desafioverity.domain.model.User
 import com.example.desafioverity.data.service.Service
 import com.example.desafioverity.domain.helpers.DataState
+import com.example.desafioverity.domain.helpers.LimitState
 import com.example.desafioverity.domain.helpers.LoadingState
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class UserRepositoryImpl @Inject constructor(private val service: Service) : UsersRepository {
+class UserRepositoryImpl @Inject constructor(private val service: Service, private val userPreferenceRepository: UserPreferenceRepository) : UsersRepository {
     override fun getAllUsers(): Flow<DataState<List<User>>> = flow {
         emit(DataState.Loading(loadingState = LoadingState.Loading))
         try {
@@ -17,13 +18,18 @@ class UserRepositoryImpl @Inject constructor(private val service: Service) : Use
                 User(
                     login = it.login,
                     id = it.id,
-                    nodeId = it.nodeId,
-                    avatarUrl = it.avatarUrl
+                    nodeId = it.nodeId?: "",
+                    avatarUrl = it.avatarUrl?: ""
                 )
             }
             emit(DataState.Data(data = users))
         } catch (error: Exception) {
             emit(DataState.Error(error = error))
+        }
+        userPreferenceRepository.getRemaining.collect {
+            if (it == "0"){
+                emit(DataState.Limit(LimitState.Limit))
+            }
         }
     }
 
@@ -34,13 +40,18 @@ class UserRepositoryImpl @Inject constructor(private val service: Service) : Use
                 User(
                     id = userDto.id,
                     login = userDto.login,
-                    nodeId = userDto.nodeId,
-                    avatarUrl = userDto.avatarUrl
+                    nodeId = userDto.nodeId?: "",
+                    avatarUrl = userDto.avatarUrl?: ""
                 )
             }
             emit(DataState.Data(data = result))
         } catch (error: Exception) {
             emit(DataState.Error(error = error))
+        }
+        userPreferenceRepository.getRemaining.collect {
+            if (it == "0"){
+                emit(DataState.Limit(LimitState.Limit))
+            }
         }
     }
 }
